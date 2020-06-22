db4e6f80ea3211102d712f082ba90cc3a7928d19
atacankullabci <atacankullabi@gmail.com>
2020-06-22 22:26:42 +0300

    JWT-Spring security integration

---
+++ src/main/java/com/atacankullabci/todoapp/common/VerificationToken.java
@@ -0,0 +1,67 @@
+package com.atacankullabci.todoapp.common;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Document
+public class VerificationToken {
+    @Id
+    private String id;
+    private String token;
+    private User user;
+    private Instant expirationDate;
+
+    public VerificationToken() {
+    }
+
+    public VerificationToken(String id, String token, User user, Instant expirationDate) {
+        this.id = id;
+        this.token = token;
+        this.user = user;
+        this.expirationDate = expirationDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Instant getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Instant expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "VerificationToken{" +
+                "id='" + id + '\'' +
+                ", token='" + token + '\'' +
+                ", user=" + user +
+                ", expirationDate=" + expirationDate +
+                '}';
+    }
+}
